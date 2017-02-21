115ac95961fb06f8fe205837d2e65e3d64b44029
alfaraday <arianafaraday@Arianas-MacBook-Pro.local>
2017-02-21 09:31:46 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+var youTubeBaseURL = 'https://www.googleapis.com/youtube/v3/search';
+
+function getData(searchTerm, callback) {
+	var query = {
+		part: 'snippet',
+		key: 'AIzaSyAsiEeJm3t2vy-RM-ePonvga20v-NkbhjE',
+		q: searchTerm
+	}
+	$.getJSON(youTubeBaseURL, query, callback);
+}
+
+function renderHTML(data) {
+	var resultElement = '';
+	console.log(data);
+	if (data.items) {
+		data.items.forEach(function(item) {
+			console.log(item);
+			resultElement += '<img src="'+item.snippet.thumbnails.default.url+'">'
+		});
+		console.log(resultElement);
+	}
+	else {
+		resultElement += '<p>No results</p>';
+	}
+	$('.results').html(resultElement);
+}
+
+function watchSubmit() {
+	$('#search-form').submit(function(event) {
+		event.preventDefault();
+		var query = $(this).find('#search-bar').val();
+		getData(query, renderHTML);
+	});
+}
+
+$(function() {watchSubmit();});
